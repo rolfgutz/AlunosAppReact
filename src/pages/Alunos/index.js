09626b276628a7f30b7bf5a05d82ef25721846d5
Rolf Gutz <RolfGutz@hotmail.com>
2022-08-16 21:55:18 -0300

    Finalizando crud via api

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,54 +1,151 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React,{useState,useEffect} from 'react';
+import { Link,useNavigate  } from 'react-router-dom'
 import './styles.css';
 import {FiEdit, FiUserX, FiXCircle} from 'react-icons/fi';
 import logoCadastro from './../../assets/logoCadastro.png'
+import api from '../../services/api';
+
 
 export default function Alunos(){
+
+    //filtrar dados 
+    const [searcInput, setSearchInput]=useState('');
+    const [filtro,setFiltro] = useState([]);
+
+
+    const navigate = useNavigate ();
+
+    const [alunos,setAlunos] = useState([]);
+
+    const email = localStorage.getItem('email');
+    const token = localStorage.getItem('token');
+
+    const authorization = {
+        headers :{
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+
+    const searchAlunos =(searcValue) => {
+        setSearchInput(searcValue);
+        if(searcInput!==''){
+            const dadosFiltrdados = alunos.filter((item)=>{
+                return Object.values(item).join('').toLocaleLowerCase()
+                .includes(searcInput.toLocaleLowerCase())
+            })
+            setFiltro(dadosFiltrdados);
+        }else{
+            setFiltro(alunos);
+        } 
+    }
+
+    useEffect(()=>{
+        api.get('api/alunos',authorization)
+        .then(response=>{setAlunos(response.data);
+        },token)
+    },[])
+
+    async function logout (){
+        try {
+            localStorage.clear();
+            localStorage.setItem('token','');
+            authorization.headers='';
+            navigate('/');           
+        } catch (error) {
+            alert('Não foi possivel fazer o logout '+ error)
+        }
+    }
+
+
+    async function editAluno(id){
+        try 
+        {
+            navigate(`/aluno/novo/${id}`);    
+        } catch (error) 
+        {
+            alert('Não foi possivel editar o aluno '+ error)
+        }
+    }
+
+
+    async function deleteAluno(id){
+        try 
+        {
+            if(window.confirm('deseja deletar o aluno de id '+id +'?'))
+            {
+                await api.delete(`api/alunos/${id}`,authorization);
+                setAlunos(alunos.filter(aluno =>aluno.id !==id));
+            }
+           
+        } catch (error) {
+            alert('Não foi possivel deletar o aluno '+ error)
+
+        }
+
+    }
+
+
     return (
         
         <div className='aluno-container'>
             <header>
                 <img src={logoCadastro} alt="Cadastro"/>
-                <span>Bem-Vindo, <strong>ROLFGUTZ</strong>!</span>
+                <span>Bem-Vindo, <strong>{email}</strong>!</span>
                 <Link className="button" to="/aluno/novo/0">Novo Aluno </Link>
-                <button type="button">
+                <button type="button" onClick={logout}>
                     <FiXCircle size={35} color="#17202a"/>
                 </button>
             </header>
             <form>
-                <input type="text" placeholder='Nome'/>
-                <button type="button" class="button-filtro"> Filtrar aluno por nome (parcial)
-                </button>
+                <input type="text" 
+                placeholder='filtrar por nome...'
+                onChange={(e) =>searchAlunos(e.target.value)}
+                />
+
+
             </form>          
             <h1>Lista de Alunos</h1>
-            <ul>
-                <li>
-                    <b>Nome:</b>Rolf<br></br>
-                    <b>Email:</b>Rolf@hotmail.com<br></br>
-                    <b>Idade:</b>30<br></br>
-                    <button type="button">
-                    <FiEdit size="25" color="#17202a"/>
-                     </button>
-                     <button type="button">
-                    <FiUserX size="25" color="#17202a"/>
-                     </button>
-                </li>
-                <li>
-                    <b>Nome:</b>Rolf<br></br>
-                    <b>Email:</b>Rolf@hotmail.com<br></br>
-                    <b>Idade:</b>30<br></br>
-                    <button type="button">
-                    <FiEdit size="25" color="#17202a"/>
-                     </button>
-                     <button type="button">
-                    <FiUserX size="25" color="#17202a"/>
-                     </button>
-                </li>
-            </ul>
-        </div>
-    )
-
-
-
+            {searcInput.length>1? 
+                (
+                <ul>
+                    {filtro.map(aluno=>(
+                        <li key={aluno.id}>
+                        <b>Nome:</b>{aluno.nome}<br></br>
+                        <b>Email:</b>{aluno.email}<br></br>
+                        <b>Idade:</b>{aluno.idade}<br></br>
+                        
+                        <button onClick={()=>editAluno(aluno.id)} type="button">
+                        <FiEdit size="25" color="#17202a" />
+                        </button>
+                        
+                        <button type="button" onClick={()=>deleteAluno(aluno.id)}>
+                            <FiUserX size="25" color="#17202a"/>
+                        </button>
+                    </li>
+                    ))}
+                </ul>
+                ):
+                (
+                    <ul>
+                        {alunos.map(aluno=>(
+                            <li key={aluno.id}>
+                            <b>Nome:</b>{aluno.nome}<br></br>
+                            <b>Email:</b>{aluno.email}<br></br>
+                            <b>Idade:</b>{aluno.idade}<br></br>
+                            
+                            <button onClick={()=>editAluno(aluno.id)} type="button">
+                            <FiEdit size="25" color="#17202a" />
+                            </button>
+                            
+                            <button type="button" onClick={()=>deleteAluno(aluno.id)}>
+                            <FiUserX size="25" color="#17202a"/>
+                            </button>
+                        </li>
+                        ))}
+                    </ul>
+                )
+            }
+            </div>
+    );
 }
--- src/pages/NovoAluno/index.js
+++ src/pages/NovoAluno/index.js
@@ -1,12 +1,82 @@
-import React from 'react';
-import { Link ,useParams} from 'react-router-dom'
+import React , {useState, useEffect} from 'react';
+import { Link ,useParams,Navigate, useNavigate} from 'react-router-dom'
 import './styles.css';
 import {FiCornerDownLeft,FiUserPlus} from 'react-icons/fi'
-import api from '../../services/api';
-
+import api from '../../services/api'
 
 export  default function NovoAluno(){
     const {alunoId} = useParams();
+
+    const [id,setId] = useState(null);
+    const [nome,setNome] = useState('');
+    const [email,setEmail] = useState('');
+    const [idade,setIdade] = useState('');
+
+    const navigate = useNavigate();
+
+    const token = localStorage.getItem('token');
+
+    const authorization = {
+        headers :{
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+
+    useEffect(()=>{
+        if(alunoId==='0')
+            return;
+        else
+        loadAluno();
+    },alunoId)
+    
+    async function loadAluno(){
+        try
+        {
+            const response = await api.get(`/api/alunos/${alunoId}`,authorization);
+            setId(response.data.id);
+            setNome(response.data.nome);
+            setEmail(response.data.email);
+            setIdade(response.data.idade);
+
+        } catch (error) 
+        {
+            alert('Erro ao recuperar aluno'+ error)
+            navigate('/alunos');
+        }
+       
+    }
+
+    async function saveOrUpdate(event){
+        event.preventDefault();
+        const data =
+        {
+            nome,
+            email,
+            idade
+        }
+
+        try
+        {
+            if(alunoId==="0"){
+                console.log('Caiu novo aluno')
+                await api.post('/api/alunos',data,authorization);
+                
+            }else
+            {
+                data.id = id;
+                console.log('Caiu Editar aluno')
+                await api.put(`/api/alunos/${id}`,data,authorization);
+                
+            }
+        } catch (error) 
+        {
+            alert('Erro ao gravar aluno'+ error)
+        }finally {
+            navigate('/alunos');
+        }
+       
+    }
 
 
 
@@ -21,10 +91,19 @@ return (
                  Retornar
                 </Link>
             </section>
-            <form>
-                <input placeholder="Nome"  />    
-                <input placeholder="Email"  />    
-                <input placeholder="Idade"  />    
+            <form onSubmit={saveOrUpdate}>
+                <input placeholder="Nome"  
+                    value={nome}
+                    onChange={e=> setNome(e.target.value)}
+                />    
+                <input placeholder="Email"  
+                    onChange={e=> setEmail(e.target.value)}
+                    value={email}
+                />    
+                <input placeholder="Idade"  
+                    onChange={e=> setIdade(e.target.value)}
+                    value={idade}
+                />    
                 <button className='button'type='submit'>{alunoId==="0"?'Inlcuir':'Atualizar'}</button>
 
             </form>
